18b910e96f6052b26d41d217eff72168305d0c10
Vidhya2k <39537600+Vidhya2k@users.noreply.github.com>
2018-12-15 23:53:52 -0800

    Removing unused references

--- TaskManagerService.Test/WhenTaskManagerServiceIsInvoked.cs
+++ TaskManagerService.Test/WhenTaskManagerServiceIsInvoked.cs
@@ -1,10 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
-using Moq;
-using NUnit.Framework;
-using TaskManagerService.Controllers;
+﻿using NUnit.Framework;
 
 namespace TaskManagerService.Test
 {
